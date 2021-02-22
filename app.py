eca371e6a891167e82fe7226082768998a0e1820
vincenthenric <vincent.henric@hotmail.fr>
2021-02-22 15:03:48 +0100

    display BBC first article

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+import feedparser
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def get_news():
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return render_template("home.html",
+                           title=first_article.get('title'),
+                           published=first_article.get('published'),
+                           summary=first_article.get('summary')
+    )
 
 if __name__ == '__main__':
     app.run()
-
