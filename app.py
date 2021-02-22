acb417700c29e5d80298476db1251e1bc19ceeda
vincenthenric <vincent.henric@hotmail.fr>
2021-02-22 16:38:17 +0100

    display all BBC articles

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 @app.route('/')
 def get_news():
     feed = feedparser.parse(BBC_FEED)
-    first_article = feed['entries'][0]
+    articles = feed['entries']
     return render_template("home.html",
-                           title=first_article.get('title'),
-                           published=first_article.get('published'),
-                           summary=first_article.get('summary')
+                           articles = articles
     )
 
 if __name__ == '__main__':
