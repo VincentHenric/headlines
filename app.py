676a4eb2b32402b6109db770b826dd030721e7b4
vincenthenric <vincent.henric@hotmail.fr>
2021-02-22 16:46:03 +0100

    add other news sources

--- app.py
+++ app.py
@@ -3,11 +3,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn':'http://rss.cnn.com/rss/edition.rss',
+             'fox':'http://feeds.foxnews.com/foxnews/latest',
+             'iol':'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route('/')
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
+@app.route('/bbc')
+def bbc():
+    return get_news('bbc')
+
+@app.route('/cnn')
+def cnn():
+    return get_news('cnn')
+
+@app.route('/fox')
+def fox():
+    return get_news('fox')
+
+@app.route('/iol')
+def iol():
+    return get_news('iol')
+
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
     articles = feed['entries']
     return render_template("home.html",
                            articles = articles
