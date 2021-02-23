fb8f6a17c642f394c00a0bfa7ebcb470e44a93df
vincenthenric <vincent.henric@hotmail.fr>
2021-02-23 15:30:19 +0100

    choose city for weather

--- app.py
+++ app.py
@@ -8,41 +8,55 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+DEFAULTS = {'publication': 'bbc',
+            'city': 'London,UK'}
 RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn':'http://rss.cnn.com/rss/edition.rss',
              'fox':'http://feeds.foxnews.com/foxnews/latest',
              'iol':'http://www.iol.co.za/cmlink/1.640'}
 WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY")
-WEATHER_FORECAST_URL = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid={}"
+WEATHER_FORECAST_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
 
-@app.route('/', methods=['GET', 'POST'])
-def get_news():
-    query = request.form.get("publication")
-    if not query or query.lower() not in RSS_FEEDS:
-        publication = "bbc"
-    else:
-        publication = query.lower()
 
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    articles = feed['entries']
+@app.route('/')
+def home():
+    publication = request.args.get("publication")
+    if not publication:
+        publication=DEFAULTS['publication']
+    articles = get_news(publication)
 
-    weather = get_weather("London,UK")
+    city = request.args.get("city")
+    if not city:
+        city=DEFAULTS['city']
+    weather = get_weather(city)
 
     return render_template("home.html",
                            articles=articles,
                            weather=weather
     )
 
+
+def get_news(query):
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = DEFAULTS['publication']
+    else:
+        publication = query.lower()
+
+    feed = feedparser.parse(RSS_FEEDS[publication])
+
+    return feed['entries']
+
 def get_weather(query):
     query = urlparse.quote(query)
-    url = WEATHER_FORECAST_URL.format(WEATHER_API_KEY)
+    url = WEATHER_FORECAST_URL.format(query, WEATHER_API_KEY)
     data = urlopen(url).read()
     parsed = json.loads(data)
     weather = None
     if parsed.get("weather"):
         weather = {"description":parsed["weather"][0]["description"],
                    "temperature":parsed["main"]["temp"],
-                   "city":parsed["name"]
+                   "city":parsed["name"],
+                   "country":parsed["sys"]["country"]
                    }
     return weather
 
